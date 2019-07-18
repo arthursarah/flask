8d6c6241c40133b2969c433805612b5c5dfb85e5
sarah arthur <arthursarah759@gmail.com>
2019-07-18 14:56:29 +0100

    hello

--- sa2.py
+++ sa2.py
@@ -1,15 +1,17 @@
-from flask import Flask,render_template,request
- 
+from flask import *
+import os
+
 app = Flask(__name__)
 
-@app.route('/add_todos')
-def todo():
+@app.route('/foo/<name>')
+def foo(name):
     
-    return render_template('form.html')
+    return render_template('index.html', to=name)
   
 @app.route('/add_todo')
 def add_todo():
     item=request.args.get('item')
     return item
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    port = os.environ.get('PORT', 5000)
+    app.run(debug=True, host='0.0.0.0',port=port)
