40a7ddd0579245f463c8464d6de2ded6d6f86a56
sarah arthur <arthursarah759@gmail.com>
2019-07-18 15:38:32 +0100

    hello

--- sa2.py
+++ sa2.py
@@ -1,17 +1,23 @@
-from flask import *
+from flask import Flask, render_template, request,redirect
 import os
-
+ 
+items =[]
 app = Flask(__name__)
-
+ 
 @app.route('/foo/<name>')
 def foo(name):
+    return render_template('form.html', to=name,items=items)
+
     
-    return render_template('index.html', to=name)
-  
+
 @app.route('/add_todo')
 def add_todo():
-    item=request.args.get('item')
-    return item
+    item =request.args.get('item')
+    items.append(item)
+    return redirect("/foo/sarah", code=302)
+
+
+
 if __name__ == '__main__':
-    port = os.environ.get('PORT', 5000)
-    app.run(debug=True, host='0.0.0.0',port=port)
+    port=os.environ.get('PORT', 5000)
+    app.run(debug=True, host='0.0.0.0', port=port)
